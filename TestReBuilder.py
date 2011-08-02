e4d8d6ea7f1109162b15e7ca4c7a6dae3a6814ce
Peter Stone <thepeterstone@gmail.com>
2011-08-02 03:02:45 +0000

    fix syntax error - missing () on self.rule()

--- TestReBuilder.py
+++ TestReBuilder.py
@@ -7,13 +7,13 @@ class TestReBuilder(unittest.TestCase):
   def testReBuilderMatchesExact(self):
     builder = ReBuilder()
     result = builder.addPattern('a');
-    assert(result.rule.match('a'))
+    assert(result.rule().match('a'))
 
   def testIncrementalAddPattern(self):
     builder = ReBuilder();
     builder.addPattern('a').addPattern('b');
-    assert(builder.rule.match('a'))
-    assert(builder.rule.match('b'))
+    assert(builder.rule().match('a'))
+    assert(builder.rule().match('b'))
 
 if __name__=='__main__':
   unittest.main()
